d8c732d1b1acd9ade732dfb3458aa672fc4d8d57
localbody macmini <localbody@gmail.com>
2026-07-14 00:43:34 +0300

    add blocks

--- dist/js/script.js
+++ dist/js/script.js
@@ -1,5 +1,44 @@
 const onLoad = () => {
-  //
+  // FAQ
+
+  const listFaqItem = document.querySelectorAll('.faq__item')
+
+  const onClickFaqItem = (event) => {
+    if (event.currentTarget.classList.contains('faq__item--active')) {
+      event.currentTarget.classList.remove('faq__item--active')
+    } else {
+      document
+        .querySelector('.faq__item--active')
+        ?.classList.remove('faq__item--active')
+      event.currentTarget.classList.add('faq__item--active')
+    }
+  }
+
+  listFaqItem.forEach((item) => {
+    item.addEventListener('click', onClickFaqItem)
+  })
+
+  // video
+  const playVideoButtons = document.querySelectorAll('.video__button-play')
+
+  const onClickPlayVideoButton = (event) => {
+    // console.log(event.target.closest('.video__content').querySelector('video'))
+
+    const videoContent = event.target.closest('.video__content')
+    const video = videoContent.querySelector('video')
+
+    videoContent.classList.add('no-after')
+    event.target.setAttribute('hidden', true)
+
+    video.setAttribute('controls', true)
+    video.play()
+  }
+
+  playVideoButtons.forEach((button) => {
+    button.addEventListener('click', onClickPlayVideoButton)
+  })
+
+  // horizontal slider
   const slider = document.querySelector('.horizontal-scroll')
 
   let isDown = false
@@ -30,7 +69,8 @@ const onLoad = () => {
     const walk = (x - startX) * 1.5
     slider.scrollLeft = scrollLeft - walk
   })
-  //
+
+  // swiper
   var swiper = new Swiper('.heroSwiper', {
     loop: true,
     navigation: {
